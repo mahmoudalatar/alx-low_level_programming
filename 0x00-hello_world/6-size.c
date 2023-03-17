65257eb7f07d1deed9f88d6226b08d240244a120
mahmoudalatar <alatar347@gmail.com>
2023-03-16 18:23:55 -0700

    Adding files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,12 +7,14 @@
 int main(void)
 {
 	int i;
+	long int li;
+	long long int lli;
 	char c;
 	float f;
 
-	printf("Size of a char: %lu byte(s)/n", (unsigned)sizeof(c));
-	printf("Size of a int: %lu byte(s)/n", (unsigned)sizeof(i));
-	printf("Size of a int long: %lu byte(s)/n", (unsigned long) sizeof(i));
-	printf("Size of a long long int: %lu byte(s)/n", (unsigned long long) sizeof(i));
-	printf("Size of a float: %lu byte(s)/n", (unsigned float) sizeof(f));
+	printf("Size of a char: %lu byte(s)/n", (unsigned long)sizeof(c));
+	printf("Size of a int: %lu byte(s)/n", (unsigned long)sizeof(i));
+	printf("Size of a int long: %lu byte(s)/n", (unsigned long) sizeof(li));
+	printf("Size of a long long int: %lu byte(s)/n", (unsigned long) sizeof(lli));
+	printf("Size of a float: %lu byte(s)/n", (unsigned long) sizeof(f));
 	return (0);
