19452f680dc2a9a8d0ea0c54a91245836786c217
mahmoudalatar <alatar347@gmail.com>
2023-03-16 17:57:38 -0700

    Adding files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 
 /**
  * main - printing with puts function
- * Return: 0 
+ * Return: 0
  */
 int main(void)
-{	
+{
 	puts("\"Programming is like building a multilingual puzzle\"");
 	return (0);
 }
