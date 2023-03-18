0cf4382b03d08b918ae88e18505e1f7ca4569ea4
mahmoudalatar <alatar347@gmail.com>
2023-03-18 11:35:33 -0700

    adding print alphabets c file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - print alphabet in lower and uppercase character
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+	char U_ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (U_ch = 'A'; U_ch <= 'Z'; U_ch++)
+        {
+                putchar(U_ch);
+        }
+	putchar('\n');
+	return (0);
+}
