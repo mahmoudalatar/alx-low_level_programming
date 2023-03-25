3b6d5c704d50be82a289f0cdc00ffb8c5ddb686d
mahmoudalatar <alatar347@gmail.com>
2023-03-25 08:42:23 -0700

    adding 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul -  multiplies two integers
+ * @a: first number will be multipile
+ * @b: second number will be multipile
+ * Return: the result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
