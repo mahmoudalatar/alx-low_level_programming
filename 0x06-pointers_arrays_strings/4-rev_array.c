7c20b8a6f162e6c096b9839d380a3a431cf9937b
mahmoudalatar <alatar347@gmail.com>
2023-03-29 09:12:15 -0700

    fixing 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,12 +8,13 @@
 void reverse_array(int *a, int n)
 {
 	int i = 0, j;
+	n -= 1;
 
 	while (i < n)
 	{
 		j = a[i];
-		a[i] = a[n - i - 1];
-		a[n - i - 1] = j;
+		a[i] = a[n];
+		a[n] = j;
 		i++;
 		n--;
 	}
