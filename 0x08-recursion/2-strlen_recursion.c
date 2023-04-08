bfd9582018b5a1c2cc177b97f81a45f3361b1876
mahmoudalatar <alatar347@gmail.com>
2023-04-08 08:08:29 -0700

    adding 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include <string.h>
+
+/**
+ * _strlen_recursion - return the length of a string
+ * @s: the string will be calc
+ * Return: int of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
