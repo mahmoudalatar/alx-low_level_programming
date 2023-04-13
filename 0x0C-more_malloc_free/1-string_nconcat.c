f6ea43e3d4830a2f80565895fbe39e9180acfd2e
mahmoudalatar <alatar347@gmail.com>
2023-04-13 15:12:29 -0700

    adding -string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: scond string
+ * @n: index of s2
+ * Return: pointer or null
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int s, i;
+	char *new_str;
+
+	if (n >= sizeof(s2))
+		s = strlen(s2);
+	else
+		s = n;
+	new_str = malloc(strlen(s1) + s);
+	if (new_str == NULL)
+		return (NULL);
+	strcpy(new_str, s1);
+	for (i = 0; i < s; i++)
+	{
+		new_str[strlen(s1) + i] = s2[i];
+	}
+	new_str[strlen(new_str)] = '\0';
+	return (new_str);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,6 +2,7 @@
 #define HEADER_FILE
 
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 
 #endif
