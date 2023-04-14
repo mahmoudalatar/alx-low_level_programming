748f59db08e262a57df568df8d5663f89cd521d1
mahmoudalatar <alatar347@gmail.com>
2023-04-13 17:27:10 -0700

    adjust 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,26 +10,28 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int s, i;
-	char *new_str;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	if (n >= sizeof(s2))
-		s = strlen(s2);
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
 	else
-		s = n;
-	new_str = malloc(strlen(s1) + s);
-	if (new_str == NULL)
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!s)
 		return (NULL);
-	strcpy(new_str, s1);
-	for (i = 0; i < s; i++)
+	while (i < len1)
 	{
-		new_str[strlen(s1) + i] = s2[i];
+		s[i] = s1[i];
+		i++;
 	}
-	new_str[strlen(new_str)] = '\0';
-	return (new_str);
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+	s[i] = '\0';
+	return (s);
 }
