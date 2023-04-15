64922bdf51b1942825cb823a6fa8c8ca475d3bdf
mahmoudalatar <alatar347@gmail.com>
2023-04-15 13:48:53 -0700

    creating nesecary object for libmy.a

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - printing
+ * @str: string
+ * Return: nothing
+ */
+
+void _puts(char *str);
+
+/**
+ * main - use as Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
