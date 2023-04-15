d4014a6ab55ba39acc379d97ba4444d3cfefeaf0
mahmoudalatar <alatar347@gmail.com>
2023-04-15 13:53:56 -0700

    modify 3-puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,19 +1,12 @@
-#include "main.h"
 #include <stdio.h>
+
 /**
- * _puts - printing
- * @str: string
+ * _puts - prints a string followed by nwe line
+ * @str: the input string
  * Return: nothing
  */
 
-void _puts(char *str);
-
-/**
- * main - use as Entry point
- * Return: Always 0 (Success)
- */
-int main(void)
+void _puts(char *str)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	puts(str);
 }
